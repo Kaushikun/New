74db8c6979f2e199206bac7a80b0e6a147a48082
Kaushikun <161507618+Kaushikun@users.noreply.github.com>
2024-08-23 11:04:13 +0530

    Update opencv.py

--- opencv.py
+++ opencv.py
@@ -1,9 +1,7 @@
 import cv2 as cv
 
-# Use the absolute path to the image
 image_path = '/home/kaushikun/Documents/photo/images.jpg'
 
-# Load and display the image
 img = cv.imread(image_path)
 cv.imshow('Image Window', img)
 cv.waitKey(0)
